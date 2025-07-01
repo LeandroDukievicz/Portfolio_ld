7388d174e2909ee67e736c91dda6fa2aebb37b34
LeandroDukievicz <ldukie@icloud.com>
2025-07-01 11:40:28 -0300

    📦 FEAT: add new font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import styles from "../styles/Home.module.css";
+
 export default function Home() {
   return (
     <>
-      <h1>Leandro Dukievicz</h1>
+      <h1 className={styles.title}>Leandro Dukievicz</h1>
     </>
   );
 }
